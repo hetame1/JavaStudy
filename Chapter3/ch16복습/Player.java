00939923be5b95d16ae5c7e8e3d68feae0bcd175
hetame1 <poi8946@naver.com>
2023-04-17 21:31:24 +0900

    1

--- Chapter3/ch16복습/Player.java
+++ Chapter3/ch16복습/Player.java
@@ -1,18 +1,25 @@
 package Chapter3.ch16복습;
 
 public class Player {
-  
-  private PlayerLevel level;
 
-  public Player() {
-    level = new PlayerLevel();
-    level
-  }
+  private PlayerLevel level;
 
   public PlayerLevel getLevel() {
     return level;
   }
 
-  
+  public Player() {
+    level = new BeginnerLevel();
+    level.showLevelMessage();
+  }
+
+  public void upgradeLevel(PlayerLevel level) {
+    this.level = level;
+    level.showLevelMessage();
+  }
+
+  public void play(int count) {
+    level.go(count);
+  }
 
 }
--- Chapter3/ch16복습/PlayerLevel.java
+++ Chapter3/ch16복습/PlayerLevel.java
@@ -1,14 +1,21 @@
 package Chapter3.ch16복습;
 
 public abstract class PlayerLevel {
-  
+
   public abstract void run();
+
   public abstract void jump();
+
   public abstract void turn();
+
   public abstract void showLevelMessage();
 
   public void go(int count) {
-    
+    run();
+    for (int i = 0; i < count; i++) {
+      jump();
+    }
+    turn();
   }
 
 }
